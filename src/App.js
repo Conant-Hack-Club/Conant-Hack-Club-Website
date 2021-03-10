3b13b8712fa4c12df8d8b615bb8b855cabadbd0a
soham gupta <gupta@Sohams-Mac-mini.local>
2021-03-09 23:55:15 -0600

    blog and navbar basic working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import { CarouselScreen } from 
+import CarouselScreen from "./components/CarouselScreen"
+import  MediaScreen from "./components/MediaScreen"
+import  Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <CarouselScreen />
+      <br></br>
+      <MediaScreen />
     </div>
   );
 }
