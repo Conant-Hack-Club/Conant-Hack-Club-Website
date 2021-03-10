442edbf8ad55c2d66d2e7b34e16c975e2a6163f9
Socksham <60550018+Socksham@users.noreply.github.com>
2021-03-09 23:54:25 -0600

    react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import { CarouselScreen } from 
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomeScreen from './components/HomeScreen'
+import ResourcesScreen from "./components/ResourcesScreen";
+import AboutUsScreen from "./components/AboutUsScreen";
+import ProjectsScreen from "./components/ProjectsScreen";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route path="/" exact component={() => <HomeScreen />}></Route>
+          <Route path="/about" exact component={() => <AboutUsScreen />}></Route>
+          <Route path="/projects" exact component={() => <ProjectsScreen />}></Route>
+          <Route path="/resources" exact component={() => <ResourcesScreen />}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CarouselScreen from './CarouselScreen'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function HomeScreen() {
+    return (
+        <div>
+            <CarouselScreen/>
+        </div>
+    )
+}
