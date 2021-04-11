e9d2fe6f384ecfc8f54e06fcf745f29467f82789
Socksham <60550018+Socksham@users.noreply.github.com>
2021-04-10 22:00:34 -0500

    logo

--- src/App.js
+++ src/App.js
@@ -9,16 +9,30 @@ import  TopNavbar from "./components/Navbar"
 
 function App() {
   return (
+    //0065ea: blue
+    //ec3750: red
     <div className="App">
       <Router>
-      <TopNavbar />
-      <br/>
-      <br/>
+        <TopNavbar />
+        <br />
+        <br />
         <Switch>
           <Route path="/" exact component={() => <HomeScreen />}></Route>
-          <Route path="/about" exact component={() => <AboutUsScreen />}></Route>
-          <Route path="/projects" exact component={() => <ProjectsScreen />}></Route>
-          <Route path="/resources" exact component={() => <ResourcesScreen />}></Route>
+          <Route
+            path="/about"
+            exact
+            component={() => <AboutUsScreen />}
+          ></Route>
+          <Route
+            path="/projects"
+            exact
+            component={() => <ProjectsScreen />}
+          ></Route>
+          <Route
+            path="/resources"
+            exact
+            component={() => <ResourcesScreen />}
+          ></Route>
         </Switch>
       </Router>
     </div>
