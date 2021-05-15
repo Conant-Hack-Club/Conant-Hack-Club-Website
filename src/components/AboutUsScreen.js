0d77bc9fc1b1d1f9952558f3519abc2a67c28082
ronit <anandani4136@students.d211.org>
2021-05-15 00:23:11 -0500

    Refinements

--- src/components/AboutUsScreen.js
+++ src/components/AboutUsScreen.js
@@ -2,35 +2,49 @@ import React from 'react'
 import '../static/css/aboutUs.css'
 
 export default function AboutUsScreen() {
-    return (
-      <div className="full__page">
-        <h1 className="about__us">About The Leaders</h1>
-        <div className="leaders__page">
-          <div className="leaders__div">
-            <div className="bio__div">
-              Ashay Parikh
-            </div>
-            <div className="pic__div">
-              <img className="person__pic" src="/ashay.jpg" alt="ASHAY"></img>
-            </div>
+
+  function clickAshay(){
+    console.log('The Ashay was clicked.');
+  }
+  function clickLabdhi(){
+    console.log('The Labdhi was clicked.');
+  }
+  function clickSohum(){
+    console.log('The Sohum was clicked.');
+  }
+
+  return (
+    <div className="full__page">
+      <br></br>
+      <br></br>
+      <br></br>
+      <h1 className="about__us">About The Leaders</h1>
+      <div className="leaders__page" >
+        <div className="leaders__div" onClick={clickAshay}>
+          <div className="bio__div">
+            Ashay Parikh
           </div>
-          <div className="leaders__div">
-            <div className="pic__div">
-              <img className="person__pic" src="/labdhi.jpg" alt="LABDHI"></img>
-            </div>
-            <div className="bio__div">
-              Labdhi Jain
-            </div>
+          <div className="pic__div">
+            <img className="person__pic" src="/ashay.jpg" alt="ASHAY"></img>
           </div>
-          <div className="leaders__div">
-            <div className="bio__div">
-              Sohum Bhole
-            </div>
-            <div className="pic__div">
-              <img className="person__pic" src="sohum.jpg" alt="SOHUM"></img>
-            </div>
+        </div>
+        <div className="leaders__div" onClick={clickLabdhi}>
+          <div className="pic__div" >
+            <img className="person__pic" src="/labdhi.jpg" alt="LABDHI" ></img>
+          </div>
+          <div className="bio__div">
+            Labdhi Jain
+          </div>
+        </div>
+        <div className="leaders__div" onClick={clickSohum}>
+          <div className="bio__div">
+            Sohum Bhole
+          </div>
+          <div className="pic__div">
+            <img className="person__pic" src="sohum.jpg" alt="SOHUM"></img>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
--- src/components/ResourcesScreen.js
+++ src/components/ResourcesScreen.js
@@ -1,23 +1,32 @@
 import React from 'react'
+import "../static/css/resourcesScreen.css";
 
 export default function ResourcesScreen() {
     return (
-        <div>
-            <h1>RESOURCES</h1>
+        <div className="screenStyle">
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <h1>Resources</h1>
             <br></br>
             <h4>AI</h4>
-            <p><a href="https://deepmind.com/blog/article/alphafold-a-solution-to-a-50-year-old-grand-challenge-in-biology "> Blog about Alphafold, which uses AI to predict protein folding. </a></p>
-            <p><a href="https://www.youtube.com/watch?v=W7wJDJ56c88&ab_channel=LexFridman"> Video explaining Alphafold </a></p>
-            <p><a href="https://medium.com/ai-in-plain-english/using-ai-to-draw-in-the-air-bd1505d1ea99"> Medium article detailing the AI Air Notability project that we did last year during Hack Club 2019-2020 second semester.  </a></p>
-            <p><a href="https://artificialintelligence-news.com/2021/01/06/openai-latest-neural-network-creates-images-written-descriptions/"> OpenAI (another AI startup that created GPT-3, the AI that could write like humans) created DALL-#, an AI that can generate images from words. </a></p>
-            <p><a href="https://medium.com/x8-the-ai-community/machine-learning-hardware-1dac168423fd"> Article on Machine Learning Hardware </a></p>
-            <p><a href="https://medium.com/machine-learning-in-practice/over-150-of-the-best-machine-learning-nlp-and-python-tutorials-ive-found-ffce2939bd78"> A LOT of Machine Learning tutorials in Python. Please check it out in your free time or weekends </a></p>
+            <p className="typer"><a href="https://deepmind.com/blog/article/alphafold-a-solution-to-a-50-year-old-grand-challenge-in-biology "> Blog about Alphafold, which uses AI to predict protein folding. </a></p>
+            <p className="typer"><a href="https://www.youtube.com/watch?v=W7wJDJ56c88&ab_channel=LexFridman"> Video explaining Alphafold </a></p>
+            <br></br>
+            <p className="typer"><a href="https://medium.com/ai-in-plain-english/using-ai-to-draw-in-the-air-bd1505d1ea99"> Medium article detailing the AI Air Notability project that we did last year during Hack Club 2019-2020 second semester.  </a></p>
+            <br></br>
+            <p className="typer"><a href="https://artificialintelligence-news.com/2021/01/06/openai-latest-neural-network-creates-images-written-descriptions/"> OpenAI (another AI startup that created GPT-3, the AI that could write like humans) created DALL-#, an AI that can generate images from words. </a></p>
+            <br></br>
+            <p className="typer"><a href="https://medium.com/x8-the-ai-community/machine-learning-hardware-1dac168423fd"> Article on Machine Learning Hardware </a></p>
+            <br></br>
+            <p className="typer"><a href="https://medium.com/machine-learning-in-practice/over-150-of-the-best-machine-learning-nlp-and-python-tutorials-ive-found-ffce2939bd78"> A LOT of Machine Learning tutorials in Python. Please check it out in your free time or weekends </a></p>
             <br></br>
             <h4>Unity</h4>
             
-            <p><a href="https://store.unity.com/download-nuo"> Download Unity </a></p>
-            <p><a href="https://www.youtube.com/watch?v=7K2SMZQRKnw"> Basics 37 min tutorial in Unity </a></p>
-            <p><a href="https://www.youtube.com/watch?v=q5ejxITvEh8"> Downloading assets </a></p>
+            <p className="typer"><a href="https://store.unity.com/download-nuo"> Download Unity </a></p>
+            <p className="typer"><a href="https://www.youtube.com/watch?v=7K2SMZQRKnw"> Basics 37 min tutorial in Unity </a></p>
+            <p className="typer"><a href="https://www.youtube.com/watch?v=q5ejxITvEh8"> Downloading assets </a></p>
 
         </div>
     )
